37bbe463e223416ca0fc753b9d497a581eb7fa8e
ogulcanucar <ogulcan.ucar@hotmail.com>
2023-05-06 00:09:17 +0300

    1.01 Login İşlemleri

---
+++ Core/ETicaretAPI.Application/Exceptions/NotFoundUserExceptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Exceptions
+{
+    public class NotFoundUserExceptions : Exception
+    {
+        public NotFoundUserExceptions():base("Kullanıcı veya Şifre Hatalı")
+        {
+        }
+
+        public NotFoundUserExceptions(string? message) : base(message)
+        {
+        }
+
+        public NotFoundUserExceptions(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotFoundUserExceptions(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using ETicaretAPI.Application.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using p = ETicaretAPI.Domain.Entities.Identity;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommandRequest, LoginUserCommandResponse>
+    {
+        readonly UserManager<p.AppUser> _userManager;
+        readonly SignInManager<p.AppUser> _signInManage;
+
+        public LoginUserCommandHandler(UserManager<p.AppUser> userManager, SignInManager<p.AppUser> signInManage)
+        {
+            _userManager = userManager;
+            _signInManage = signInManage;
+        }
+
+        public async Task<LoginUserCommandResponse> Handle(LoginUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            p.AppUser user = await _userManager.FindByNameAsync(request.UsernameOrEmail);
+            if (user == null)
+                user = await _userManager.FindByEmailAsync(request.UsernameOrEmail);
+            if (user == null)
+                throw new NotFoundUserExceptions("Kullanıcı veya Şifre Hatalı....");
+            SignInResult result = await _signInManage.CheckPasswordSignInAsync(user, request.Password, false);
+            if (result.Succeeded) //Authentication başarılı
+            { //yetkileri belirlememiz gerekiyor
+            }
+            return new();
+
+        }
+    }
+}
